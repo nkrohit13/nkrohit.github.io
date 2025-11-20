796647f35be3d487f47728e16e70969d0d28bdca
Nirmal Rohit <nirmalrohit333@gmail.com>
2025-11-20 06:18:00 +0530

    WIP: local fixes before syncing with remote

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://nkrohit13.github.io",
-    base: '/nkrohit/',
+    base: '/',
 });
