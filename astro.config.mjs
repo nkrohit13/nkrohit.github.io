445354ac3a8f1152d9cb4b62ff57140b9fd0f512
Nirmal Rohit <nirmalrohit333@gmail.com>
2025-11-20 05:59:47 +0530

    Configure site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind()
-    ]
+    site: "https://nkrohit13.github.io",
+    base: '/nkrohit/',
 });
